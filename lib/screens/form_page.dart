1cb17aefefcd27f46b17089236c8a02398d0f956
May <May@hotmail.com>
2019-09-30 13:41:51 +0700

    buttoncamera gallery done

--- lib/screens/form_page.dart
+++ lib/screens/form_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart'; //import puglin กล้อง
 
 class FormPage extends StatefulWidget {
   @override
@@ -6,8 +9,94 @@ class FormPage extends StatefulWidget {
 }
 
 class _FormPageState extends State<FormPage> {
+  
+  // ประกาศตัวแปร
+  File file;
+  
+
+  // Method
+
+  //รูป
+  Widget showImage() {
+    return Container(
+      margin: EdgeInsets.only(top: 10.0),
+      height: 200.0,
+      child: file == null ? Image.asset('images/noimage.png') : Image.file(file) ,
+    );
+  }
+
+// แถวแสดงปุ่ม
+  Widget showButton() {
+    return Container(
+      width: 200.0,
+      // color: Colors.yellow,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          cameraButton(),
+          galleryButton(),
+        ],
+      ),
+    );
+  }
+
+  //ปุ่มCamera
+  Widget cameraButton() {
+    return IconButton(
+      icon: Icon(
+        Icons.add_a_photo,
+        size: 48.0,
+        color: Colors.greenAccent,
+      ),
+      onPressed: () {
+        cametaThread();
+      },
+    );
+  }
+
+
+//Thread Camera
+  Future<void> cametaThread() async{
+
+    var imageObject = await ImagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+      file = imageObject; 
+    });
+
+  }
+
+  //ปุ่มGallery
+  Widget galleryButton() {
+    return IconButton(
+      icon: Icon(
+        Icons.add_photo_alternate,
+        size: 48.0,
+        color: Colors.red,
+      ),
+      onPressed: () {
+        galleryThread();
+      },
+    );
+  }
+
+  //Thread Gallery
+  Future<void> galleryThread() async{
+
+    var imageObject = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      file = imageObject; 
+    });
+
+  }
+
+//////////////////////////////////////////////////////////////////////
   @override
   Widget build(BuildContext context) {
-    return Text('This is Form Page');
+    return ListView(
+      children: <Widget>[
+        showImage(),
+        showButton(),
+      ],
+    );
   }
 }
